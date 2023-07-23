bb779c3178cfab381eb56d7eb4ebc6ff7ec7a90e
FireLord <firelord.xda@gmail.com>
2023-07-23 15:32:52 +0530

    app: Setup login with email and google

--- app/src/main/java/com/firelord/login/LoginFragment.kt
+++ app/src/main/java/com/firelord/login/LoginFragment.kt
@@ -1,14 +1,41 @@
 package com.firelord.login
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.findNavController
 import com.firelord.login.databinding.FragmentLoginBinding
+import com.firelord.login.util.Constants.Companion.RC_SIGN_IN
+import com.firelord.login.util.Constants.Companion.TAG
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginFragment : Fragment() {
     private lateinit var loginBinding: FragmentLoginBinding
+    // Progress Dialog
+    private lateinit var progressDialog: ProgressDialog
+
+    // FireBase Auth
+    private lateinit var firebaseAuth : FirebaseAuth
+    private var email = ""
+    private var password = ""
+
+    // Google Auth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,6 +48,141 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // config progress dialog
+        progressDialog = ProgressDialog(requireContext())
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("Logging in..")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        // call google
+        val googleSignInOption = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(),googleSignInOption)
+
+        // init firebaseauth
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        // Google Sign in button
+        loginBinding.btGoogle.setOnClickListener {
+            Log.d(TAG,"onCreate: begin google SignIn")
+            val intent = googleSignInClient.signInIntent
+            startActivityForResult(intent, RC_SIGN_IN)
+        }
+
+        // handle click, open sign up page
+        loginBinding.tvNoAccount.setOnClickListener {
+            it.findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
+        }
+
+        // handle click, begin login
+        loginBinding.btLogin.setOnClickListener{
+            //before logging in, validate data
+            validateData()
+        }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // Request returned from launching the intent
+        if (requestCode == RC_SIGN_IN){
+            Log.d(TAG,"onActivityResult: Google sign in intent")
+            val accountTask = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign in success
+                val account = accountTask.getResult(ApiException::class.java)
+                firebaseAuthWithGoogleAccount(account)
+            }
+            catch (e:Exception){
+                // failed google signin
+                Log.d(TAG,"onActivityResult: ${e.message}")
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogleAccount(account: GoogleSignInAccount?) {
+        Log.d(TAG,"firebaseAuthWithGoogleAccount: begin firebase auth with google")
+        val credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnSuccessListener { authResult ->
+                //login success
+                Log.d(TAG,"firebaseAuthWithGoogleAccount: LoggedIn")
+
+                // Get logged in user
+                val firebaseUser = firebaseAuth.currentUser
+
+                // get user id
+                val uid = firebaseUser!!.uid
+                val email = firebaseUser!!.email
+
+                Log.d(TAG,"firebaseAuthWithGoogleAccount: uid: ${uid}")
+                Log.d(TAG,"firebaseAuthWithGoogleAccount: email: ${email}")
+
+                //check if user is new or existing
+                if (authResult.additionalUserInfo!!.isNewUser){
+                    //user is new - account created
+                    Log.d(TAG,"firebaseAuthWithGoogleAccount: Account created ...\n$email")
+                    Toast.makeText(requireContext(),"Account created $email",Toast.LENGTH_SHORT).show()
+                } else {
+                    // existing user - logged in
+                    Log.d(TAG,"firebaseAuthWithGoogleAccount: Existing user \n$email")
+                    Toast.makeText(requireContext(), "Logged in as $email",Toast.LENGTH_SHORT).show()
+                }
+                // open MainActivity
+                loginBinding.root.findNavController().navigate(R.id.action_loginFragment_to_profileFragment)
+            }
+            .addOnFailureListener { e ->
+                Log.d(TAG,"firebaseAuthWithGoogleAccount: Login failed due to ${e.message}")
+                Toast.makeText(requireContext(), "Login failed due to ${e.message}",Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun validateData() {
+        email = loginBinding.etEmail.text.toString().trim()
+        password = loginBinding.etPass.text.toString().trim()
+
+        // validate data
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            // invalid email format
+            loginBinding.etEmail.error = "Invalid email format"
+        }
+        else if (TextUtils.isEmpty(password)){
+            // no password entered
+            loginBinding.etPass.error = "please enter password"
+        }
+        else {
+            // data is validated, begin login
+            firebaseLogin()
+        }
+    }
+
+    private fun firebaseLogin() {
+        // show progress
+        progressDialog.show()
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+            .addOnSuccessListener {
+                //login success
+                progressDialog.dismiss()
+                //get user info
+                val firebaseUser = firebaseAuth.currentUser
+                val email = firebaseUser!!.email
+                Toast.makeText(requireContext(), "Logged in as $email",Toast.LENGTH_SHORT).show()
+                // open mainActivity
+                loginBinding.root.findNavController().navigate(R.id.action_loginFragment_to_profileFragment)
+            }
+            .addOnFailureListener { e ->
+                // login failed
+                progressDialog.dismiss()
+                Toast.makeText(requireContext(), "Login failed due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun checkUser(){
+        val  firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser!=null){
+           loginBinding.root.findNavController().navigate(R.id.action_loginFragment_to_profileFragment)
+        }
+    }
 }
--- app/src/main/java/com/firelord/login/SignupFragment.kt
+++ app/src/main/java/com/firelord/login/SignupFragment.kt
@@ -1,14 +1,29 @@
 package com.firelord.login
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.findNavController
 import com.firelord.login.databinding.FragmentSignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignupFragment : Fragment() {
     private lateinit var signupBinding: FragmentSignupBinding
+    // Progress Dialog
+    private lateinit var progressDialog: ProgressDialog
+
+    // firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var email = ""
+    private var password = ""
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,5 +35,64 @@ class SignupFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // config progress dialog
+        progressDialog = ProgressDialog(requireContext())
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("creating account in..")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        // init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+        // handle click, begin sign up
+        signupBinding.btSignUp.setOnClickListener {
+            //validate data
+            validateData()
+        }
+    }
+    private fun validateData() {
+        // get data
+        email = signupBinding.etEmailSignUp.text.toString().trim()
+        password = signupBinding.etPassSignUp.text.toString().trim()
+
+        // validate data
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            // invalid email format
+            signupBinding.etEmailSignUp.error = "Invalid email format"
+        }
+        else if (TextUtils.isEmpty(password)){
+            // no password entered
+            signupBinding.etPassSignUp.error = "please enter password"
+        }
+        else if (password.length <6){
+            // password length is less than 6
+            signupBinding.etPassSignUp.error = "Password must be atleast 6 char long"
+        }
+        else {
+            // data is validated, continue sign up
+            firebaseSignUp()
+        }
+    }
+
+    private fun firebaseSignUp() {
+        progressDialog.show()
+
+        // create account
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+            .addOnSuccessListener {
+                //signup success
+                progressDialog.dismiss()
+                // get current user
+                val firebaseUser = firebaseAuth.currentUser
+                val email = firebaseUser!!.email
+                Toast.makeText(requireContext(),"Account created with email $email",Toast.LENGTH_SHORT).show()
+
+                // open MainActivity
+                signupBinding.root.findNavController().navigate(R.id.action_signupFragment_to_profileFragment)
+            }
+            .addOnFailureListener { e->
+                //sign up failed
+                progressDialog.dismiss()
+                Toast.makeText(requireContext(),"Sign up Failed due to ${e.message}",Toast.LENGTH_SHORT).show()
+            }
     }
 }

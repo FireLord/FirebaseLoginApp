4eee47c06e28508f310e6b4350ec23879dae302b
FireLord <firelord.xda@gmail.com>
2023-07-23 14:29:48 +0530

    app: setup navigation

--- app/src/main/java/com/firelord/login/MainActivity.kt
+++ app/src/main/java/com/firelord/login/MainActivity.kt
@@ -2,10 +2,14 @@ package com.firelord.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.firelord.login.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
     }
 }
